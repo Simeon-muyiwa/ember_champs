08778d622120f4a39072121df1a4a00063636f80
Simeon Muyiwa <muyiwa100@yahoo.com>
2015-08-09 12:34:26 +0100

    add user posts

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :posts,                only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
